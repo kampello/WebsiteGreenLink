d267fe0271f7b36b1403ad601d3db381700894bd
ricardo <ricardoaascunha@gmail.com>
2025-12-15 20:06:58 +0000

    3 Colunas de infromação colocadas

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,45 +1,103 @@
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    title: 'Vegetais Frescos e Locais',
+    images: [
+      '/img/greenlink_vegetais1.jpg',
+      '/img/tomato.jpg',
+      '/img/brocoli.jpg',
+    ],
+    description:
+      'Colhidos diariamente por agricultores locais, garantindo frescura, sabor e qualidade máxima.',
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    title: 'Sustentabilidade em Primeiro Lugar',
+    images: [
+      '/img/AIMG1.jpg',
+      '/img/AIMG2.jpeg',
+      '/img/AIMG3.jpeg',
+    ],
+    description:
+      'A Greenlink apoia práticas agrícolas responsáveis, reduzindo o impacto ambiental e promovendo o consumo consciente.',
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    title: 'Encomendas Rápidas e Práticas',
+    images: [
+      '/img/AIMG4.jpg',
+      '/img/AIMG5.jpg',
+      '/img/AIMG6.jpg',
+    ],
+    description:
+      'Escolha os seus vegetais, receba a encomenda no seu negócio e acompanhe todo o processo online.',
+  },
+  {
+    title: 'Rastreabilidade dos Produtos',
+    images: [
+      '/img/AIMG7.jpg',
+      '/img/AIMG8.jpg',
+      '/img/AIMG9.jpg',
+    ],
+    description:
+      'Saiba exatamente de onde vêm os seus produtos, quem os produziu e como chegaram até si.',
+  },
+  {
+    title: 'Apoio ao Produtor Local',
+    images: [
+      '/img/AIMG10.jpg',
+      '/img/AIMG11.jpg',
+      '/img/AIMG12.jpg',
+    ],
+    description:
+      'Valorizamos pequenos e médios agricultores, promovendo uma economia mais justa e sustentável.',
+  },
+  {
+    title: 'Qualidade Garantida',
+    images: [
+      '/img/AIMG13.jpg',
+      '/img/AIMG14.jpg',
+      '/img/AIMG15.jpg',
+    ],
+    description:
+      'Todos os produtos passam por controlo de qualidade para garantir segurança alimentar e excelência.',
   },
 ];
 
-function Feature({Svg, title, description}) {
+function ImageCarousel({ images, title }) {
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 2500);
+
+    return () => clearInterval(timer);
+  }, [images.length]);
+
   return (
-    <div className={clsx('col col--4')}>
+    <div className={styles.carousel}>
+      {images.map((src, index) => (
+        <img
+          key={index}
+          src={src}
+          alt={`${title} ${index + 1}`}
+          className={clsx(styles.carouselImage, {
+            [styles.active]: index === current,
+          })}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Feature({ title, description, images }) {
+  return (
+    <div className={clsx('col col--4 margin-bottom--lg')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <ImageCarousel images={images} title={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
@@ -53,10 +111,27 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
+        {/* TÍTULO DA SEÇÃO */}
+        <div className="text--center margin-bottom--xl">
+          <Heading as="h2">Porquê escolher a Greenlink?</Heading>
+          <p className={styles.subtitle}>
+            Conectamos produtores locais a negócios de forma sustentável,
+            rápida e transparente.
+          </p>
+        </div>
+
+        {/* FEATURES */}
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
+        </div>
+
+        {/* CALL TO ACTION */}
+        <div className="text--center margin-top--xl">
+          <a className="button button--primary button--lg" href="/docs/intro">
+            Começar agora
+          </a>
         </div>
       </div>
     </section>
